64fc957e6aeba9757b76f7c3bbde2e5b3a312f1d
Ranjithmaddy <42378754+Ranjithmaddy@users.noreply.github.com>
2025-12-04 15:35:13 +0100

    Update ranjithplan.tf

--- ranjithplan.tf
+++ ranjithplan.tf
@@ -1,6 +1,67 @@
+data "aap_job_template" "deploy_web_server" {
+  name              = "Deploy Web Server"
+  organization_name = var.organization_name
+}
+
+data "aap_job_template" "deploy_web_site" {
+  name              = "Deploy Web Site"
+  organization_name = var.organization_name
+}
+
+
 resource "aap_inventory" "inventory" {
   name         = "${var.instance_name_prefix}-aap-inventory"
-  description  = "A new inventory name"
+  description  = "A new inventory"
   organization = 2
+}
+
+# Add the new EC2 instance to the inventory
+resource "aap_host" "my_host" {
+  for_each     = { for idx, instance in aws_instance.web_server : idx => instance }
+  inventory_id = aap_inventory.inventory.id
+  groups = toset([resource.aap_group.my_group.id])
+  name         = each.value.tags.Name
+  description  = "Host provisioned by HCP Terraform"
+  variables    = jsonencode({
+    ansible_user = "ec2-user"
+    ansible_host = each.value.public_ip
+    #public_ip    = each.value.public_ip
+    #target_hosts = each.value.public_ip
+  })
+}
+
+# Create some infrastructure - inventory group - that has an action tied to it
+resource "aap_group" "my_group" {
+  name = "role_webserver"
+  inventory_id = aap_inventory.inventory.id
+}
+
+resource "aap_job" "deploy_web_server" {
+  inventory_id = aap_inventory.inventory.id
+  job_template_id = data.aap_job_template.deploy_web_server.id
+  wait_for_completion                 = true
+  wait_for_completion_timeout_seconds = 180
+  triggers = {
+    "aap_job_run_timestamp":timestamp()
+  }
+  depends_on = [
+    aap_host.my_host,
+    aap_group.my_group
+  ]
+}
+
+resource "aap_job" "deploy_web_site" {
+  inventory_id = aap_inventory.inventory.id
+  job_template_id = data.aap_job_template.deploy_web_site.id
+  wait_for_completion                 = true
+  wait_for_completion_timeout_seconds = 180
+
+  triggers = {
+    "aap_job_run_timestamp":timestamp()
+  }
+
+  depends_on = [
+    aap_job.webserver
+  ]
 }
 
