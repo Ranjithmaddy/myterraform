e71fa6d0853892a53658d54c06e83a02fb884ef2
Ranjithmaddy <42378754+Ranjithmaddy@users.noreply.github.com>
2025-12-04 14:05:27 +0100

    Update ranjithplan.tf

--- ranjithplan.tf
+++ ranjithplan.tf
@@ -1,4 +1,4 @@
-resource "aap_inventory" "ranjith_inventory" {
+resource "aap_inventory" "ranjith_inventory_new" {
   name         = "My new inventory"
   description  = "A new inventory for testing"
   organization = 2
