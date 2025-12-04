0e2eb056958930ccf5298606bf1180f6c94fdecc
Ranjithmaddy <42378754+Ranjithmaddy@users.noreply.github.com>
2025-12-04 13:47:09 +0100

    Update ranjithplan.tf

--- ranjithplan.tf
+++ ranjithplan.tf
@@ -1,1 +1,11 @@
+resource "aap_inventory" "ranjith_inventory" {
+  name         = "My new inventory"
+  description  = "A new inventory for testing"
+  organization = 2
+  variables = jsonencode(
+    {
+      "foo" : "bar"
+    }
+  )
+}
 
